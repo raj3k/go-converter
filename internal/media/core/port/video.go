5334aae352eaf36145698a62d0dc4e6c3174df16
raj3k <rajkosky@gmail.com>
2024-07-03 20:58:05 +0200

    update type of videoID

--- internal/media/core/port/video.go
+++ internal/media/core/port/video.go
@@ -2,18 +2,19 @@ package port
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/raj3k/go-converter/internal/media/core/domain"
 	"io"
 )
 
 type VideoService interface {
 	UploadVideo(ctx context.Context, video *domain.Video, file io.Reader) (*domain.Video, error)
-	GetVideoByID(ctx context.Context, videoID string) (*domain.Video, error)
-	DeleteVideo(ctx context.Context, videoID string) error
+	GetVideoByID(ctx context.Context, videoID uuid.UUID) (*domain.Video, error)
+	DeleteVideo(ctx context.Context, videoID uuid.UUID) error
 }
 
 type VideoRepository interface {
 	StoreVideo(ctx context.Context, video *domain.Video, file io.Reader) (*domain.Video, error)
-	GetVideoByID(ctx context.Context, videoID string) (*domain.Video, error)
-	DeleteVideo(ctx context.Context, videoID string) error
+	GetVideoByID(ctx context.Context, videoID uuid.UUID) (*domain.Video, error)
+	DeleteVideo(ctx context.Context, videoID uuid.UUID) error
 }
